5f9ad8af6d231b7370afd078a3d9eb2a38c841d1
minhtoanfpt <117805081+minhtoanfpt@users.noreply.github.com>
2023-11-01 20:26:28 +0700

    spam login

--- app/Http/Controllers/Client/LoginController.php
+++ app/Http/Controllers/Client/LoginController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -59,28 +61,35 @@ class LoginController extends Controller
         if ($result) {
             //lấy nhiều ip trong 1 tài khoản
             $ip_user = $result->ip;
+            $count_ip = count($ip_user);
         } else {
             $ip_user = null;
         }
         // Đếm số ip cử 1 tài khoản
-        $count_ip = count($ip_user);
+       
         if (Auth::attempt($request->only('email', 'password'))) {
+            //Kiểm tra thông tin trong ip user có ip đó chưa
             if (!(in_array($ip, $ip_user))) {
+                //Kiểm tra số lượng ip người dùng đã đến giới hạn chưa
                 if ($count_ip < $limit) {
                     User::where('email', $findEmail)->push(
                         'ip',
                         [$request->ip()]
                     );
                 } else {
+                    //Bắt lỗi tài khoảng cộng đồng
                     User::where('email', $findEmail)->update(
                         ['role' => ['652a9a45835ceedb746a99ef']]
                     );
                 }
             }
+           
         } else {
+
             return redirect()->back()->withInput($request->only('email'))->withErrors([
-                'email' => 'Thông tin đăng nhập không đúng.',
+                'email' => 'Thông tin đăng nhập không đúng. '
             ]);
+
         }
         return redirect()->route('home')->with('success', 'Đăng nhập thành công');
     }
