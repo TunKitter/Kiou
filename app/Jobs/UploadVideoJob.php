3d3e1d9a5d41dfd2397915f495f5628a5f9d4c99
Tunkit <tunkitjava@gmail.com>
2023-12-05 08:10:21 +0700

    fix url kiou google cloud config path

--- app/Jobs/UploadVideoJob.php
+++ app/Jobs/UploadVideoJob.php
@@ -2,13 +2,12 @@
 
 namespace App\Jobs;
 
+use Google\Cloud\Storage\StorageClient;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Google\Cloud\Storage\StorageClient;
 use Illuminate\Support\Facades\Http;
 
 class UploadVideoJob implements ShouldQueue
@@ -20,10 +19,10 @@ class UploadVideoJob implements ShouldQueue
      */
 
     //  protected $path;
-     protected $fileName;
-     protected $mentorId;
-     protected $course_id;
-    public function __construct( $fileName, $mentorId,$course_id)
+    protected $fileName;
+    protected $mentorId;
+    protected $course_id;
+    public function __construct($fileName, $mentorId, $course_id)
     {
         // $this->path = $path;
         $this->fileName = $fileName;
@@ -37,24 +36,21 @@ class UploadVideoJob implements ShouldQueue
     public function handle(): void
     {
         //  Upload to google
-       
+
         $storage = new StorageClient([
-            'keyFilePath' =>__DIR__.'kiou_bucket_key.json',
+            'keyFilePath' => __DIR__ . '\..\..\kiou_bucket_key.json',
         ]);
         $bucketName = 'kiou_lesson';
-       
+
         $bucket = $storage->bucket($bucketName);
-                
-        $bucket->upload(fopen(storage_path('app\public\videos\\'.$this->fileName),'r'), ['name' => $this->mentorId.'/'. $this->course_id . '/'.$this->fileName]);
 
-        Http::asForm()->post('http://127.0.0.1:8000/api/convertVideo',[
-            'url' => 'https://storage.googleapis.com/'.$bucketName.'/'.$this->mentorId.'/'. $this->course_id.'/'.$this->fileName,
-            'name' =>  str_replace('.mp4', '', $this->fileName),
-           
+        $bucket->upload(fopen(storage_path('app\public\videos\\' . $this->fileName), 'r'), ['name' => $this->mentorId . '/' . $this->course_id . '/' . $this->fileName]);
+
+        Http::asForm()->post('http://127.0.0.1:8000/api/convertVideo', [
+            'url' => 'https://storage.googleapis.com/' . $bucketName . '/' . $this->mentorId . '/' . $this->course_id . '/' . $this->fileName,
+            'name' => str_replace('.mp4', '', $this->fileName),
+
         ]);
-       
-
-
 
     }
 }
